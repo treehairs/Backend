41863f67b13e90e458312349b312bc604667324b
treehairs <treehairs@foxmail.com>
2024-11-07 14:45:36 +0800

    取消跟踪 .key 文件”

--- server/models/db.js
+++ server/models/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 // 创建数据库连接
 const connection = mysql.createConnection({
-    host: 'mysql.sqlpub.com',
-    user: 'hlg_admin',
-    password: 'ZIK6i3Plw4AClOCY',
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
     database: 'conveniencemart'
 });
 
